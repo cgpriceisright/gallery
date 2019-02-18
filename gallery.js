aa49523fb9ab64b4dbea234cee586d9fa44eb7e7
Chris <chrisprice1993@aol.com>
2019-02-17 23:45:43 -0500

    add transitions javascript

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 const carouselSlide = document.querySelector('.carousel-Slide');
-const carouselImages = document.querySelector('.carousel-Slide img')
+const carouselImages = document.querySelector('.carousel-Slide img');
 
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
@@ -10,6 +10,23 @@ const size = carouselImages[0].clientWidth;
 carouselSlide.style.transform = 'translateX(' + (size * counter) + 'px';
 
 nextBtn.addEventlistener('click',()=>{
+  if
   carouselSlide.style.transition = "transform 0.5s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (size * counter) + 'px';
 
 });
+
+prevBtn.addEventlistener('click',()=>{
+  carouselSlide.style.transition = "transform 0.5s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (size * counter) + 'px';
+
+});
+
+carouselSlide.addEventlistener('transitionend',)()=>{
+if (carouselImages[counter].id==='lastClone')
+    carousel.Slide.style.transition='none';
+    counter = carouselImages.length -2;
+    carouselSlide.style.transform='translateX('+ (-size*counter)+'px)';
+}
