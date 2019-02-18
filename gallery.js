6d1fe323af9605aca3ae44a311b2d09fcc3b2043
Chris <chrisprice1993@aol.com>
2019-02-17 23:28:40 -0500

    add java slide transform

--- gallery.js
+++ gallery.js
@@ -1,58 +1,15 @@
- // document ready function
+const carouselSlide = document.querySelector('.carousel-Slide');
+const carouselImages = document.querySelector('.carousel-Slide img')
 
-	//DEFINE YOUR VARIABLES
-	//select all the slides (value of the variable will be an array)
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
-	//set the index slide (number that starts on 0)
+let counter = 1;
+const size = carouselImages[0].clientWidth;
 
-	//set current slide based on index
+carouselSlide.style.transform = 'translateX(' + (size * counter) + 'px';
 
+nextBtn.addEventlistener('click',()=>{
+  carouselSlide.style.transition = "transform 0.5s ease-in-out";
 
-	//STEPS
-	//1. stack slides together OR hide all slides except for the one we want to see. (e.g., all but the first)
-		//a.
-		//b.
-	//2. move in the specified direction
-		//a.
-		//b.
-		//c. return to the opposite or resetting after you click thru each image and reach the end of the slideshow
-
-	//INSTRUCTIONS
-	//hide all the slides
-
-	//show the first slide
-
-	//when the user clicks "next"
-
-		//hide the current slide
-
-
-		//show the next
-	slideIndex++; // 
-		//same as slideIndex=
-		//slideindex+1
-		
-		//conditional check of the slideIndex (if greater than 6 bc there are 7 photos in the slideshow..or whatever is the last one)
-
-		//reset to 0 (there are 7 photos, but 0 is the first number so it's technically "return after 6")
-		//go to the start
-
-	//when the user clicks "previous"
-
-		slideIndex--; // 
-		//same as slideIndex=
-		//slideindex-1
-		
-		//conditional check of the slideIndex (if greater than 6 bc there are 7 photos in the slideshow..or whatever is the last one)
-
-		//reset to 6 (there are 7 photos, but 0 is the first number so it's technically "return after 6")
-		//go to the start
-
-
-		//go to the end
-
-
-
-
-
-
+});
